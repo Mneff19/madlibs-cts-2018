174f112de8a6eae8911932cc2f2d0ae406d8e044
Mneff19 <mpneff19@gmail.com>
2018-07-05 10:01:13 -0600

    7/5

--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Input = (title, valueGuy, func) => {
+const Input = ({title, state, name}, onChange, index) => {
   return (
-    <div className="input">
-      <input value={valueGuy} onChange={func} />
-      <label>{title}</label>
+    <div key={index} className='inputs'>
+      <label className='inputs__number'>{index + 1}</label>
+      <input value={state} onChange={onChange} name={name}/>
+      <label className='inputs__title'>{title}</label>
     </div>
-  )
+  );
 }
 
 export default Input;
